5301369770f3aa4f9ab9185366c5be968524a740
Kieran Singh <kieransingh27@gmail.com>
2022-07-19 23:20:33 +1000

    Switched to using express

--- index.js
+++ index.js
@@ -1,43 +1,20 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express')
+const app = express();
 
-const serverPort = 8080;
-const urlBase = "http://localhost:8080/"
-let filePath = "./"
-let statusCode = 200;
-
-// Initialise Web Server
-const server = http.createServer((req, res) => {
- console.log(req.url);
-  switch(req.url) {
-    case "/":
-      filePath = `./index.html`
-      statusCode = 200;
-      break;
-    case "/contact":
-      filePath = `./contact-me.html`
-      statusCode = 200;
-      break;
-    case "/about":
-      filePath = `./about.html`
-      statusCode = 200;
-      break;
-    default:
-      filePath = `./404.html`
-      statusCode = 404;
-      break;
-  }
-
-  fs.readFile(filePath, (err, data) => {
-    if (err) throw err;
-    res.writeHead(statusCode, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  })
+app.get('/', (req, res) => {
+  res.sendFile("./index.html", { root: __dirname })
 })
 
-// Listen on server
-server.listen(serverPort, () => {
-    console.log("Server running at port " + serverPort + "...");
-});
+app.get('/about', (req, res) => {
+  res.sendFile("./about.html", { root: __dirname })
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile("./contact-me.html", { root: __dirname })
+})
+
+app.get('*', (req, res) => {
+  res.sendFile("./404.html", { root: __dirname })
+})
+
+app.listen(8080)
